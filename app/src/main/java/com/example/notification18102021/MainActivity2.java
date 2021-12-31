de7b476be8028bbe25463987b0b5326de2dfd8c6
phamtanphat <phatdroid94@gmail.com>
2021-12-31 18:24:44 +0700

    special activity

--- app/src/main/java/com/example/notification18102021/MainActivity2.java
+++ app/src/main/java/com/example/notification18102021/MainActivity2.java
@@ -19,6 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     Button mBtnOpen, mBtnClose;
     String CHANNEL_ID = "mychannel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,25 +31,26 @@ public class MainActivity2 extends AppCompatActivity {
         mBtnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this,MainActivity2.class);
-                TaskStackBuilder stackBuilder = TaskStackBuilder.create(MainActivity2.this);
-                stackBuilder.addNextIntentWithParentStack(intent);
+                Intent intent = new Intent(MainActivity2.this, MainActivity2.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+                        MainActivity2.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+                );
 
-                PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(MainActivity2.this,CHANNEL_ID);
+                NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(MainActivity2.this, CHANNEL_ID);
                 notificationCompat.setSmallIcon(R.mipmap.ic_launcher);
                 notificationCompat.setContentTitle("Thông báo app có phiên bản mới");
                 notificationCompat.setContentText("Phiên bản 2.5 vừa được cập nhật");
                 notificationCompat.setShowWhen(true);
-                notificationCompat.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+ "://" + getPackageName()+"/"+R.raw.musicdata));
-                notificationCompat.setContentIntent(pendingIntent);
+                notificationCompat.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/" + R.raw.musicdata));
+                notificationCompat.setContentIntent(notifyPendingIntent);
 //                notificationCompat.addAction(R.drawable.ic_launcher_background,"Open App" ,pendingIntent);
 
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,"New_Version",NotificationManager.IMPORTANCE_DEFAULT);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "New_Version", NotificationManager.IMPORTANCE_DEFAULT);
 //                    Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://"+ getPackageName() + "/" + R.raw.musicdata);
 //                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
 //                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
@@ -58,7 +60,7 @@ public class MainActivity2 extends AppCompatActivity {
                     notificationManager.createNotificationChannel(notificationChannel);
                 }
 
-                notificationManager.notify(1 , notificationCompat.build());
+                notificationManager.notify(1, notificationCompat.build());
             }
         });
 
